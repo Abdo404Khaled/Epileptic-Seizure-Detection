544975e1597016fa7f487500cc6c55c635f38506
Abdelrahman Mahmoud <mado4335@gmail.com>
2024-07-10 04:10:33 +0300

    leave one out, modules added

---
+++ helper/modules.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+from scipy.signal import resample
+from scipy.interpolate import interp1d
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.model_selection import train_test_split
+from imblearn.under_sampling import RandomUnderSampler
+
+class EpilepticSeizure():
+    def __init__(self, config):
+        self.config = config
+        self.X = None
+        self.y = None
+    
+    def process(self):
+        self.__read_data()
+        self.__augment_data()
+
+        return self.X, self.y
+
+    def __read_data(self):
+        data = pd.read_csv(self.config['data_path'])
+
+        self.X = data.iloc[:,1:-1]
+        self.y = data.iloc[:,-1:]
+
+        self.y = self.y['y'].apply(lambda x: 1 if x == 1 else 0)
+        self.y = pd.DataFrame(data=self.y)
+    
+    def __augment_data(self):
+        X_list = [self.X]
+        y_list = [self.y]
+
+        x_columns = self.X.columns
+        y_columns = self.y.columns
+
+        if self.config.get('add_noise', False):
+            X_augmented_noise, y_augmented_noise = self.__add_noise()
+            X_list.append(X_augmented_noise)
+            y_list.append(y_augmented_noise)
+
+        if self.config.get('oversampling', False):
+            X_over_resampled, y_over_resampled = self.__over_sampling()
+            X_list.append(X_over_resampled)
+            y_list.append(y_over_resampled)
+
+        if self.config.get('undersampling', False):
+            X_under_resampled, y_under_resampled = self.__under_sampling()
+            X_list.append(X_under_resampled)
+            y_list.append(y_under_resampled)
+
+        self.X = np.vstack(X_list)
+        self.y = np.concatenate(y_list)
+
+        self.X = pd.DataFrame(self.X, columns=x_columns)
+        self.y = pd.DataFrame(self.y, columns=y_columns)
+
+    
+    def __add_noise(self):
+        augmented_data = []
+
+        for idx, row in self.X.iterrows():
+            data = row.values
+            label = self.y.iloc[idx]
+
+            noise_level = np.random.uniform(0, 0.1)
+            noisy_data = data + noise_level * np.random.normal(size=len(data))
+
+            factor = np.random.uniform(0.9, 1.1)
+            x = np.arange(len(noisy_data))
+            f = interp1d(x, noisy_data)
+            new_x = np.linspace(0, len(noisy_data) - 1, int(len(noisy_data) * factor))
+            interpolated_data = f(new_x)
+
+            resampled_data = resample(interpolated_data, len(data))
+            augmented_data.append(np.append(resampled_data, label))
+
+        augmented_data = np.array(augmented_data)
+
+        np.random.shuffle(augmented_data)
+
+        X_augmented_noise = pd.DataFrame(augmented_data[:, :-1], columns=self.X.columns)
+        y_augmented_noise = pd.DataFrame(augmented_data[:, -1], columns=['y'])
+    
+        return X_augmented_noise, y_augmented_noise
+        
+    def __over_sampling(self):
+        return RandomOverSampler(random_state=42).fit_resample(self.X, self.y)
+
+    def __under_sampling(self):
+        return RandomUnderSampler(random_state=42).fit_resample(self.X, self.y)
+
+#####################################################################################################################
+#TODO: Modify for this dataset
+class LeaveOneOut():
+    def __init__(self, dataset, model, subsets_no, prediction_label):
+        self.dataset = data
+        self.patient_subjects = []
+        self.model = model
+        self.patient_no = subsets_no
+        self.prediction_label = prediction_label
+        self.data_len = len(self.dataset)
+        self.num_rows_per_df = self.data_len // self.patient_no
+        
+        for i in range(self.patient_no):
+            start_index = i * self.num_rows_per_df
+            end_index = (i + 1) * self.num_rows_per_df
+            subject = dataset.iloc[start_index:end_index]
+            self.patient_subjects.append(subject)
+        
+    def __split(self, selected_index):
+        test_data = self.patient_subjects[selected_index]
+        train_data = [self.patient_subjects[i] for i in range(self.patient_no) if i != selected_index]
+        train_labels = [i for i in range(self.patient_no) if i != selected_index]
+        train_data = pd.concat(train_data, ignore_index=True)
+        print(f"Leaving patient {selected_index} out, training on patients {', '.join(map(str, train_labels))}")
+        X_train = train_data.drop(self.prediction_label, axis = 1)
+        y_train = train_data[self.prediction_label]
+        X_test = test_data.drop(self.prediction_label, axis = 1)
+        y_test = test_data[self.prediction_label]
+        return X_train, y_train, X_test, y_test
+        
+    def train(self, epoch, batch_size, validation_split = 0, callbacks=[]):
+        results = []
+        for selected_index in range(self.patient_no):
+            X_train, y_train, X_test, y_test = self.__split(selected_index)
+            model = self.model()
+            print('Training....')
+            train_history = model.fit(X_train, y_train, epochs = epoch, batch_size = batch_size, callbacks=callbacks, validation_split = validation_split)
+            print('Test....')
+            test_accuracy = model.evaluate(X_test, y_test, batch_size = batch_size)
+            print()
+            results.append((train_history, test_accuracy))
+        return results
